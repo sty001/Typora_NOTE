5750b872e68c45072d75962fa402847c7cafe06d
sty001 <lcmg.sty@gmail.com>
2022-03-20 23:14:38 +0800

    更新说明

--- python学习笔记/NOTE_Programming_python/001/dump_db_classes.py
+++ python学习笔记/NOTE_Programming_python/001/dump_db_classes.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@文件    :dump_db_classes.py
+@邮箱    :lcmg.sty@gmail.com
+@时间    :2022/03/19 16:45:17
+@作者    :lcmg_STY
+@版本    :1.0
+'''
 import shelve
 db = shelve.open('class-shelve')
 for key in db:
--- python学习笔记/NOTE_Programming_python/001/dump_db_file.py
+++ python学习笔记/NOTE_Programming_python/001/dump_db_file.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@文件    :dump_db_file.py
+@邮箱    :lcmg.sty@gmail.com
+@时间    :2022/03/19 16:45:49
+@作者    :lcmg_STY
+@版本    :1.0
+'''
 from make_db_file import loadDbase
 db = loadDbase()
 for key in db:
